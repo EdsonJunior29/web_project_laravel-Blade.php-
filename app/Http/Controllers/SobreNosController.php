acdee70c2c5d599eecd44fe27042f3d4fc90eeff
Edson Junior <edson.junior@mereo.com>
2023-12-02 19:50:55 -0300

    Controllers
    Adding controllers to execute logic when calling the route from the frontend

--- app/Http/Controllers/SobreNosController.php
+++ app/Http/Controllers/SobreNosController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class SobreNosController extends Controller
 {
-    //
+    public function index()
+    {
+        echo 'SOBRE-NÓS';
+    }
 }
