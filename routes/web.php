74648b38a5145d4c93bfe6ca2b626ad1183eee93
Edson Junior <edson.junior@mereo.com>
2023-12-02 19:40:57 -0300

    Create routes and controllers
    Creating the first web routes and their controllers.
    The controllers we use the php artisan make:controller ContatoController
    command to create each one of them.

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/sobre-nos', function () {
+    return 'SOBRE-NOS';
+});
+
+Route::get('/contato', function () {
+    return 'CONTATO';
+});
