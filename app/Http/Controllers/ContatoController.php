09b940f8956a93751944623a41a75b1536ebe552
Edson Junior <edson.junior@mereo.com>
2023-12-04 09:44:18 -0300

    Forwards parameter to views
    Sends the parameters received in the controllers to the views.

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -11,6 +11,15 @@ class ContatoController extends Controller
 
     public function teste(int $p1, int $p2)
     {
-        return $p1 + $p2;
+        //  Array assosiativo
+       // return view('site.teste',['p1' => $p1, 'p2' => $p2 ]);
+
+        // função compact
+        //return view('site.teste',compact('p1', 'p2'));
+
+        // função with
+        return view('site.teste')
+            ->with('p1', $p1)
+            ->with('p2', $p2);
     }
 }
