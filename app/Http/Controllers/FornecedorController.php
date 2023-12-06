01136cc05264d4783725c029443e49e00d679d28
Edson Junior <edson.junior@mereo.com>
2023-12-05 21:12:16 -0300

    @if / @else
    Working with if and else in the laravel(blade) framework

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -8,6 +8,12 @@ class FornecedorController extends Controller
 {
     public function index()
     {
-        return view('app.fornecedor.index');
+        $fornecedores = ['Fornecedor 1'];
+        $clientes = 10;
+        return view('app.fornecedor.index',
+             compact(
+                'clientes',
+                'fornecedores'
+            ));
     }
 }
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,1 +1,13 @@
 <h3>Fornecedor Index</h3>
+
+@if($clientes > 5)
+    <h3>Temos mais de 5 cliente</h3>
+@endif
+
+@if(count($fornecedores) > 0 && count($fornecedores) < 5)
+    <h3>Temos poucos fornecedores</h3>
+@elseif (count($fornecedores) > 5)
+    <h3>Temos mais de 5 fornecedores</h3>
+@else
+    <h3>Temos mais de 10 fornecedores</h3>
+@endif
