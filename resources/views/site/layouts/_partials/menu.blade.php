70011d662ea58db7c8c5f950f6b6f851084e0d56
Edson Junior <edson.junior@mereo.com>
2023-12-12 21:37:01 -0300

    Blade @include
    Performs the inclusion of a view within another view

---
+++ resources/views/site/layouts/_partials/menu.blade.php
@@ -0,0 +1,12 @@
+<div class="topo">
+    <div class="logo">
+        <img src="{{ asset('img/logo.png') }}">
+    </div>
+    <div class="menu">
+        <ul>
+            <li><a href="/">Principal</a></li>
+            <li><a href="/sobre-nos">Sobre Nós</a></li>
+            <li><a href="/contato">Contato</a></li>
+        </ul>
+    </div>
+</div>
--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -2,18 +2,6 @@
 
 @section('title', 'Home')
 @section('conteudo')
-<div class="topo">
-    <div class="logo">
-        <img src="{{ asset('img/logo.png') }}">
-    </div>
-    <div class="menu">
-        <ul>
-            <li><a href="/">Principal</a></li>
-            <li><a href="/sobre-nos">Sobre Nós</a></li>
-            <li><a href="/contato">Contato</a></li>
-        </ul>
-    </div>
-</div>
 
 <div class="conteudo-destaque">
 
