8f68bc2fac3df497fb5a18ed24582237979845ef
Edson Junior <edson.junior@mereo.com>
2023-12-20 21:19:29 -0300

    Adding the name of the table that is being presented in the database to the model

--- app/Models/Filial.php
+++ app/Models/Filial.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Filial extends Model
 {
     use HasFactory;
+
+    protected $table = 'filiais';
 }
--- app/Models/ProdutoDetalhe.php
+++ app/Models/ProdutoDetalhe.php
@@ -5,9 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Unidade extends Model
+class ProdutoDetalhe extends Model
 {
     use HasFactory;
 
-    protected $table = 'unidades';
+    protected $table = 'produto_detalhes';
 }
