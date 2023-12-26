810e3e9dbc18d52807321c027c35e39f83325920
Edson Junior <edson.junior@mereo.com>
2023-12-26 20:08:07 -0300

    Creating middleware to save logs

--- app/Http/Middleware/LogAcessMiddleware.php
+++ app/Http/Middleware/LogAcessMiddleware.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\LogAcesso;
 use Closure;
 use Illuminate\Http\Request;
 
@@ -16,6 +17,10 @@ class LogAcessMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
+        $ip = $request->server->get('REMOTE_ADDR');
+        $route = $request->getRequestUri();
+        LogAcesso::create(['log' => "IP $ip Acessou a rota $route"]);
+
         return $next($request);
     }
 }
